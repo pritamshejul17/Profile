6d2c2bb02012376647673478c3aa08a8280359d3
Pritam Shejul <117113144+pritamshejul17@users.noreply.github.com>
2023-03-11 12:26:41 +0530

    Made it Responsive

--- src/App.js
+++ src/App.js
@@ -9,10 +9,10 @@ function App() {
     <>
       <nav> 
         <ul className = "flex justify-center pt-10 pb-4 border-b-2 border-blacks mx-40">
-          <li className = "text-lg font-normal ml-10 text-gray-600"><Link to = "/">Home</Link></li>
-          <li className = "text-lg font-normal ml-10 text-gray-600"><Link to = "/projects">Projects</Link></li>
-          <li className = "text-lg font-normal ml-10 text-gray-600"><Link to={{ pathname: "https://drive.google.com/file/d/1IGoqdCpEnt-vVVyFjHfhIFsgHSm_5neT/view?usp=sharing" }} target="_blank">Resume</Link></li>
-          <li className = "text-lg font-normal ml-10 text-gray-600"><Link to = "/about">About</Link></li>
+          <li className = "text-lg font-normal ml-5 sm:ml-10 text-gray-600"><Link to = "/">Home</Link></li>
+          <li className = "text-lg font-normal ml-5 sm:ml-10 text-gray-600"><Link to = "/projects">Projects</Link></li>
+          <li className = "text-lg font-normal ml-5 sm:ml-10 text-gray-600"><Link to={{ pathname: "https://drive.google.com/file/d/1IGoqdCpEnt-vVVyFjHfhIFsgHSm_5neT/view?usp=sharing" }} target="_blank">Resume</Link></li>
+          <li className = "text-lg font-normal ml-5 sm:ml-10 text-gray-600"><Link to = "/about">About</Link></li>
         </ul>
       </nav>
 
--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -4,8 +4,8 @@ export default function Projects() {
     return (
         <>
             <h1 className = "text-center text-xl font-medium p-8 text-sky-900">PROJECTS</h1>
-            <div className = "flex justify-center items-center">
-                <a  href = "https://github.com/pritamshejul17/premierleague19" target = "_blank" className = "border-r-4 border-white p-10 bg-gray-200">
+            <div className = "flex flex-col sm:flex-row justify-center items-center">
+                <a  href = "https://github.com/pritamshejul17/premierleague19" target = "_blank" className = "border-t-4 sm:border-r-4 border-white p-10 bg-gray-200 w-full sm:w-auto">
                     <h1 className = "text-center text-sky-900 text-xl font-medium px-2 pt-4">Premier League 19</h1>
                     <h4 className = "text-center text-sm p-2 text-gray-600">SELF PROJECT</h4>
                     <p className = "p-6 text-gray-800 text-center">Application where you can checkout <br/> 
@@ -13,7 +13,7 @@ export default function Projects() {
                     season.</p>
                     <h1 className = "text-center p-2 border-y-2 border-white font-medium text-sky-900">Spring Boot, React JS</h1>
                 </a>
-                <a href =  "https://github.com/pritamshejul17/React-To-Do-App" target = "_blank" className = "border-r-4 border-white p-10 bg-gray-200">
+                <a href =  "https://github.com/pritamshejul17/React-To-Do-App" target = "_blank" className = "border-t-4 sm:border-r-4 border-white p-10 bg-gray-200 w-full sm:w-auto">
                     <h1 className = "text-center text-sky-900 text-xl font-medium pt-4">To Do's App</h1>
                     <h4 className = "text-center text-sm p-2 text-gray-600">SELF PROJECT</h4>
                     <p className = "p-6 text-gray-800 text-center">Application to keep track of your <br/> 
@@ -21,7 +21,7 @@ export default function Projects() {
                     React JS.</p>
                     <h1 className = "text-center p-2 border-y-2 border-white font-medium text-sky-900">React JS, Tailwind CSS</h1>
                 </a>
-                <a href =  "https://github.com/pritamshejul17/VS_Clone" target = "_blank" className = " p-10 bg-gray-200" >
+                <a href =  "https://github.com/pritamshejul17/VS_Clone" target = "_blank" className = "border-t-4 sm:border-r-4 border-white p-10 bg-gray-200 w-full sm:w-auto" >
                     <h1 className = "text-center text-sky-900 text-xl font-medium px-4 pt-4">VS Clone</h1>
                     <h4 className = "text-center text-sm p-2 text-gray-600">SELF PROJECT</h4>
                     <p className = "p-6 text-gray-800 text-center">Created clone of VS code website <br/> 
