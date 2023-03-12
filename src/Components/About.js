3f64f722d8ac0b5395e170f1db5a009cae7706e6
Pritam Shejul <117113144+pritamshejul17@users.noreply.github.com>
2023-03-12 23:44:46 +0530

    Made minor changes

--- src/Components/About.js
+++ src/Components/About.js
@@ -10,7 +10,7 @@ export default function About() {
                     frontend <span className = "font-medium text-sky-900">React JS</span> and related libraries, you can checkout my personal <br />
                     projects on my <a href = "https://github.com/pritamshejul17/" target = "_blank" className = "font-medium text-sky-900" >Github</a>
                 </p>
-                <h1 className = "p-6 mt-2">Thank you for your time visitng my profile, you can reach out to me at <a href = "mailto:pritamshejul17@gmail.com" className = "font-medium text-sky-900">pritamsheul17@gmail.com</a>
+                <h1 className = "p-6 mt-2">Thank you for your time visitng my profile, you can reach out to me at <a href = "mailto:pritamshejul17@gmail.com" className = "font-medium text-sky-900">pritamshejul17@gmail.com</a>
                 </h1>
 
                 <h1 className = "pt-4 font-medium text-sky-900">Have a Nice Day!</h1>
--- src/Components/Main.js
+++ src/Components/Main.js
@@ -5,7 +5,7 @@ export default function Main() {
             <div className = "flex flex-col justify-center p-12 text-gray-600 order-last">
                 <h1 className = "flex text-3xl font-normal text-sky-900 justify-center mb-3">Pritam Shejul</h1>
                 <p className = "text-center p-4 text-lg">Hi I'm Pritam! I am a software engineer with bachelor's in information<br />technology,
-                I love building web appplications & I am  <br />passionate about solving real world problems.</p>
+                I love building web applications & I am  <br />passionate about solving real world problems.</p>
             </div>
             <div className = " flex justify-center sm:order-last">
                 <img src = {profile} alt ="" className = "h-80 w-80 rounded-full p-2"/>
