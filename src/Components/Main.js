4164dcdf4390df1c3c55a9fcd0703cdda38d871c
Pritam Shejul <117113144+pritamshejul17@users.noreply.github.com>
2023-04-12 12:09:59 +0530

    Refactored

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -3,9 +3,9 @@ export default function Main() {
     return (
         <main className = "flex flex-col items-center sm:items-stretch sm:flex-row sm:justify-evenly mt-6">
             <div className = "flex flex-col justify-center p-12 text-stone-600 order-last">
-                <h1 className = "flex text-3xl font-normal text-sky-900 justify-center mb-3">Pritam Shejul</h1>
-                <p className = "text-center p-4 text-lg">Hi I'm <span className = 'font-medium text-sky-700'>Pritam</span> ! I am a software engineer with bachelor's in information<br />technology,
-                I am passionate about building web applications <br /> & solving real world problems.</p>
+                <h1 className = "flex text-3xl font-normal text-sky-900 justify-center mb-3">Pritam  Shejul</h1>
+                <p className = "text-center p-4 text-lg">A skilled Java Programmer, who enjoys solving complex challenges.
+                <br />As a problem solver, I thrive on discovering new solutions<br /> & continuously improving processes and systems.</p>
             </div>
             <div className = " flex justify-center sm:order-last">
                 <img src = {profile} alt ="" className = "h-80 w-80 rounded-full p-2"/>
