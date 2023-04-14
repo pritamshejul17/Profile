524becc6f6d925d0e069425eef832ef795051b38
Pritam Shejul <117113144+pritamshejul17@users.noreply.github.com>
2023-04-14 19:06:00 +0530

    Changed icons

--- src/Components/Connect.js
+++ src/Components/Connect.js
@@ -5,12 +5,12 @@ import mailto from '../img/mailto1.png'
 export default function Connect() {
     return (
         <div className = "h-full p-6">
-            <h1 className = "text-center p-1 py-2 text-stone-600 font-medium">Connect With Me!</h1>
+            <h1 className = "text-center text-sm p-1 py-2 text-stone-600 font-medium">Connect With Me!</h1>
             <ul className = "flex justify-center p-2">
-                <a href = "https://github.com/pritamshejul17" target = "_blank"><img className = "h-10 w-10 rounded-full ml-4" alt ="github logo" src = {github}></img></a>
-                <a href = "https://www.linkedin.com/in/pritam-shejul/" target = "_blank"><img className = "h-10 w-10 rounded-full ml-4" alt = "linkedin logo" src = {linkedin}></img></a>
-                <a href = "https://leetcode.com/pritam17/" target = "_blank"><img className = "h-10 w-10 rounded-full ml-4 bg-gray-100 border-2 border-gray-400" alt = "" src = {leetcode}></img></a>
-                <a href = "mailto:pritamshejul17@gmail.com"><img className = "h-10 w-10 rounded-full ml-4 " alt = "mailto logo" src = {mailto}></img></a>
+                <a href = "https://github.com/pritamshejul17" target = "_blank"><img className = "h-8 w-8  ml-4" alt ="github logo" src = "https://cdn.jsdelivr.net/npm/simple-icons@3.13.0/icons/github.svg"></img></a>
+                <a href = "https://leetcode.com/pritam17/" target = "_blank"><img className = "h-8 w-8  ml-4 " alt = "" src = "https://cdn.jsdelivr.net/npm/simple-icons@3.13.0/icons/leetcode.svg"></img></a>
+                <a href = "https://www.linkedin.com/in/pritam-shejul/" target = "_blank"><img className = "h-8 w-8  ml-4" alt = "linkedin logo" src = "https://cdn.jsdelivr.net/npm/simple-icons@3.13.0/icons/linkedin.svg"></img></a>
+                <a href = "mailto:pritamshejul17@gmail.com"><img className = "h-8 w-8  ml-4 " alt = "mailto logo" src = "https://cdn.jsdelivr.net/npm/simple-icons@3.13.0/icons/gmail.svg"></img></a>
             </ul>
         </div>
     );
--- src/Components/Main.js
+++ src/Components/Main.js
@@ -2,7 +2,7 @@ import profile from '../img/profile.JPG'
 export default function Main() {
     return (
         <main className = "flex flex-col items-center sm:items-stretch sm:flex-row sm:justify-evenly mt-6">
-            <div className = "flex flex-col justify-center p-12 text-stone-600 order-last">
+            <div className = "flex flex-col justify-center p-12 text-stone-800 order-last">
                 <h1 className = "flex text-3xl font-normal text-sky-900 justify-center mb-3">Pritam  Shejul</h1>
                 <p className = "text-center p-4 text-lg">A skilled <span className ='text-sky-900 font-medium'>Java Programmer</span>, who enjoys solving complex challenges.
                 <br />As a problem solver, I thrive on discovering new solutions<br /> & continuously improving processes and systems.</p>
